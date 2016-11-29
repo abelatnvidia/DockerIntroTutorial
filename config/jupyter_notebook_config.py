b9c3bea236b67270b14a0cb2b2ef2519759fb1d4
Ubuntu <ubuntu@ip-172-31-13-33.ec2.internal>
2016-11-29 16:41:08 +0000

    added media and config

---
+++ config/jupyter_notebook_config.py
@@ -0,0 +1,5 @@
+c.NotebookApp.certfile = '/home/ec2-user/.jupyter/certs/mycert.pem'
+c.NotebookApp.ip = '*'
+c.NotebookApp.open_browser = False
+c.NotebookApp.password = 'sha1:fa13c7782769:f9961e06498f7394fd18374acf8fe5adac986de6'
+c.NotebookApp.port = 8888
